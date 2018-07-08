91fbdd4ba820921f1066b4a04673a1c1e868639d
FenrirRD <38294346+FenrirRD@users.noreply.github.com>
2018-07-08 16:31:15 -0300

    Add files via upload

--- submarino.rb
+++ submarino.rb
@@ -12,20 +12,13 @@ class Submarino
     @path  = 'assets/submarino.png' if string == 'l'
     case string
     when 'u'
-      puts @y
       @y -= 5 if @y > 100
     when 'd'
-      #puts @y
       @y += 5 if @y < 540
-      #puts @y
     when 'l'
-      #puts @y
       @x -= 5 if @x > 5
-      #puts @y
     when 'r'
-      #puts @y
       @x += 5 if @x < 730
-      #puts @y
     end
   end
 
@@ -92,28 +85,50 @@ class Submarino
   def hasMergulhador(mergulhador)
     right = @x + 75
     left = @x
-    up = @y
-    down = @y + 60
+    up = @y + 10
+    down = @y + 70
 
-    if (left >= mergulhador.x && left <= mergulhador.x + 60)
-      if (up >= mergulhador.y && up <= mergulhador.y + 40)
+    #mesma coluna e x perto
+    if (mergulhador.x >= left && (mergulhador.x + 60) <= right)
+      if (mergulhador.y <= down && (mergulhador.y + 40) >= down)
         return true
       end
 
-      if (down >= mergulhador.y && down <= mergulhador.y + 40)
+      if (mergulhador.y <= up && (mergulhador.y + 40) >= up)
+        return true
+      end
+    end
+    # mesma linha e y perto
+    if (mergulhador.y >= up && (mergulhador.y + 40) <= down)
+      if (mergulhador.x <= left && (mergulhador.x + 60) >= left)
+        return true
+      end
+
+      if (mergulhador.x <= right && (mergulhador.x + 60) >= right)
+        return true
+      end
+    end
+    #canto superior direito
+    if (mergulhador.y <= up && (mergulhador.y + 40) >= up)
+      if (mergulhador.x <= left && (mergulhador.x + 60) >= left)
+        return true
+      end
+
+      if (mergulhador.x <= right && (mergulhador.x + 60) >= right)
+        return true
+      end
+    end
+    #mesma coluna e x perto
+    if (mergulhador.x >= left && (mergulhador.x + 60) <= right)
+      if (mergulhador.y <= down && (mergulhador.y + 40) >= down)
+        return true
+      end
+
+      if (mergulhador.y <= up && (mergulhador.y + 40) >= up)
         return true
       end
     end
 
-    if (right >= mergulhador.x && right <= mergulhador.x + 60)
-      if (up >= mergulhador.y && up <= mergulhador.y + 40)
-        return true
-      end
-
-      if (down >= mergulhador.y && down <= mergulhador.y + 40)
-        return true
-      end
-    end
     if (mergulhador.x >= 810 || mergulhador.x <= -10)
       return true
     end
