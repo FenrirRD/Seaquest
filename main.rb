8271cc744e86d4679c8ed0835715c4b24ab86d26
FenrirRD <38294346+FenrirRD@users.noreply.github.com>
2018-07-08 12:54:35 -0300

    Update main.rb

--- main.rb
+++ main.rb
@@ -40,27 +40,29 @@ update do
   else
     mergulhador << Mergulhador.new(x: (740), y: rand(600), l: m) if (rand(1000) % 666).zero?
   end
-#  peixe << Peixe.new(x: rand(720), y: rand(520)) if (rand(1000) % 666).zero?
-#  mergulhador << Mergulhador.new(x: rand(720), y: rand(520)) if (rand(1000) % 333).zero?
 
   peixe.each(&:draw)
   mergulhador.each(&:draw)
 
-  #peixe @x += 10
   peixe.each(&:mov)
   mergulhador.each(&:mov)
 #  pointer = Driver::readDriver
+#  @pointer
 #  puts pointer.read_string
-#  robot.walk(pointer.read_string)
+#  submarino.walk(pointer.read_string)
 
   peixe.delete_if { |b| submarino.hasPeixe(b) }
 #  peixe.delete_if { |b| peixe.delet(b)}
   mergulhador.delete_if { |b| submarino.hasMergulhador(b) }
 #  mergulhador.delete_if { |b| mergulhador.delet(b)}
-end
 
-on :key_down do |event|
-  close if event.key == 'escape'
+  on :key_down do |event|
+    close if event.key == 'escape'
+    submarino.walk('u') if event.key == 'w' || event.key == 'up'
+    submarino.walk('d') if event.key == 's' || event.key == 'down'
+    submarino.walk('l') if event.key == 'a' || event.key == 'left'
+    submarino.walk('r') if event.key == 'd' || event.key == 'right'
+  end
 end
 
 show
